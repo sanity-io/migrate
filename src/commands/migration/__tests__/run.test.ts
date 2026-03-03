6e5d4b2915c0623369c0e7c3da120946fa4f74fe
Binoy Patel <me@binoy.io>
2026-03-03 09:55:29 -0500

    test: remove snapshot tests for command help (#47)

--- src/commands/migration/__tests__/run.test.ts
+++ src/commands/migration/__tests__/run.test.ts
@@ -1,6 +1,5 @@
 import {readdir} from 'node:fs/promises'
 
-import {runCommand} from '@oclif/test'
 import {testCommand} from '@sanity/cli-test'
 import {afterEach, beforeAll, beforeEach, describe, expect, test, vi} from 'vitest'
 
@@ -119,59 +118,6 @@ describe('#migration:run', () => {
   })
   afterEach(() => {
     vi.clearAllMocks()
-  })
-
-  test('--help works', async () => {
-    const {stdout} = await runCommand(['migration run', '--help'])
-    expect(stdout).toMatchInlineSnapshot(String.raw`
-      "Run a migration against a dataset
-
-      USAGE
-        $ sanity migration run [ID] [--api-version <value>] [--concurrency
-          <value>] [--confirm] [--dataset <value>] [--dry-run] [--from-export <value>]
-          [--progress] [--project <value>]
-
-      ARGUMENTS
-        [ID]  ID
-
-      FLAGS
-        --api-version=<value>  API version to use when migrating. Defaults to
-                               v2024-01-29.
-        --concurrency=<value>  [default: 6] How many mutation requests to run in
-                               parallel. Must be between 1 and 10. Default: 6.
-        --[no-]confirm         Prompt for confirmation before running the migration
-                               (default: true). Use --no-confirm to skip.
-        --dataset=<value>      Dataset to migrate. Defaults to the dataset configured
-                               in your Sanity CLI config.
-        --[no-]dry-run         By default the migration runs in dry mode. Use
-                               --no-dry-run to migrate dataset.
-        --from-export=<value>  Use a local dataset export as source for migration
-                               instead of calling the Sanity API. Note: this is only
-                               supported for dry runs.
-        --[no-]progress        Display progress during migration (default: true). Use
-                               --no-progress to hide output.
-        --project=<value>      Project ID of the dataset to migrate. Defaults to the
-                               projectId configured in your Sanity CLI config.
-
-      DESCRIPTION
-        Run a migration against a dataset
-
-      EXAMPLES
-        dry run the migration
-
-          $ sanity migration run <id>
-
-        execute the migration against a dataset
-
-          $ sanity migration run <id> --no-dry-run --project xyz --dataset staging
-
-        execute the migration using a dataset export as the source
-
-          $ sanity migration run <id> --from-export=production.tar.gz --no-dry-run \
-            --project xyz --dataset staging
-
-      "
-    `)
   })
 
   test('errors when user only enters projectId flag', async () => {
